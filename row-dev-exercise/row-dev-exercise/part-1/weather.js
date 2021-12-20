1a6d01322220faef8a78a945ef6983e3fb5c26d7
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-12-19 21:06:19 -0500

    added js to weather.js

---
+++ row-dev-exercise/row-dev-exercise/part-1/weather.js
@@ -0,0 +1,15 @@
+const url='https://restofworld.org/wp-content/projects/code/exercise/data/weather.js';
+fetch(url , {
+    mode:'no-cors',
+    method: "post",
+    headers: {
+         "Content-Type": "application/json"
+    },
+    body: JSON.stringify(ob)
+}
+.then(response=> response.json())
+.then(data => {
+})
+    .catch(() => {
+        msg.textContent= "Search for the weather conditions in Palo Alto";
+})
