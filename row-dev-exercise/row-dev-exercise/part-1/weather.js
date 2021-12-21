003c6070ecb8116e2050d2a0dc82e39b172c38d3
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-12-20 23:16:33 -0500

    moved js to html page, and finally got json to load

--- row-dev-exercise/row-dev-exercise/part-1/weather.js
+++ row-dev-exercise/row-dev-exercise/part-1/weather.js
@@ -1,21 +1,22 @@
-window.addEventListener('load',() =>{
-const url='https://restofworld.org/wp-content/projects/code/exercise/data/weather.js';
-fetch(url , {
-    mode:'no-cors',
-    method: "post",
-    headers: {
-         "Content-Type": "application/json"
-    },
-    // body: JSON.stringify(ob)
+// window.addEventListener('load',() =>{
+// const url='https://restofworld.org/wp-content/projects/code/exercise/data/weather.js';
+// fetch(url , {
+//     mode:'no-cors',
+//     method: "post",
+//     headers: {
+//          "Content-Type": "application/json"
+//     },
+//     // body: JSON.stringify(ob)
 
 
-    .then((response) => { response.json()
- .then((data) => {
-    console.log(data.sentence);
-    document.querySelector("#content").innerText = data.sentence;
-    // const city= data.name;
-    // const {temp} = data.main;
- }
-  });
-});
-});
+// }
+// .then(response => response.json())
+//     .then(data => {
+//     })
+//     .catch(()=> {
+//         msg.textContent="Please search for a valid city";
+//     // const city= data.name;
+//     // const {temp} = data.main;
+//     }));
+// });
+
