9906cf8488bf9b7122d7889908758fad91e2f0cc
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-12-23 16:49:20 -0500

    wrote code for array and objects to be listed via a for loop

--- row-dev-exercise/row-dev-exercise/part-1/weather.js
+++ row-dev-exercise/row-dev-exercise/part-1/weather.js
@@ -1,37 +1,23 @@
-// window.addEventListener('load',() =>{
+//  window.addEventListener('load',() =>{
 // const url='https://restofworld.org/wp-content/projects/code/exercise/data/weather.js';
-// fetch(url , {
-//     mode:'no-cors',
+//  fetch('https://restofworld.org/wp-content/projects/code/exercise/data/weather.js');{
+//          mode:'no-cors',
+//          method: "post",
+//          headers: {
+//          "Content-Type": "application/json"
+//  }
+//         .then(function(resp){
+//             return resp.json();
+//         })
+//         .then(function(data){
+//             console.log(data);
+//         });
+//  });
+ //     mode:'no-cors',
 //     method: "post",
 //     headers: {
 //          "Content-Type": "application/json"
-//     },
+
 //     // body: JSON.stringify(ob)
 
-
-// }
-// .then(response => response.json())
-//     .then(data => {
-//     })
-//     .catch(()=> {
-//         msg.textContent="Please search for a valid city";
-//     // const city= data.name;
-//     // const {temp} = data.main;
-//     }));
-// });
-
-
-// window.onload = function(){
-//     const obj={geoloc: "city", limit:15 };
-//     let sc = document.createElement("script");
-//     sc.src='https://restofworld.org/wp-content/projects/code/exercise/data/weather.js '
-//     + JSON.stringify(obj);
-//      document.body.appendChild("#content");
-//     function myFunc(myObj){
-//         let txt="";
-//         for(let x in myObj){
-//             txt += myObj[x].name + "<br>"
-//         }
-//         document.getElementById("content").innerHTML = txt;
-//     }
-// }
+ 
