0d6435a2753506f0e439edc96d7882b683374227
Kimberly Suares <kimberlyjsuares@gmail.com>
2021-12-29 22:39:58 -0500

    Added column, flex, and width and height to css

--- row-dev-exercise/row-dev-exercise/part-1/weather.js
+++ row-dev-exercise/row-dev-exercise/part-1/weather.js
@@ -1,23 +1,60 @@
-//  window.addEventListener('load',() =>{
-// const url='https://restofworld.org/wp-content/projects/code/exercise/data/weather.js';
-//  fetch('https://restofworld.org/wp-content/projects/code/exercise/data/weather.js');{
-//          mode:'no-cors',
-//          method: "post",
-//          headers: {
-//          "Content-Type": "application/json"
-//  }
-//         .then(function(resp){
-//             return resp.json();
-//         })
-//         .then(function(data){
-//             console.log(data);
-//         });
-//  });
- //     mode:'no-cors',
-//     method: "post",
-//     headers: {
-//          "Content-Type": "application/json"
-
-//     // body: JSON.stringify(ob)
+    
+//     var weatherJsonpCallback= function(data){
+//      console.log(data);
+//     const cities = data.cities;
 
  
+//     // var sc = document.createElement("script");
+//     // sc.src="https://restofworld.org/wp-content/projects/code/exercise/data/weather.js? callback= + weatherJsonpCallback";
+//     //     console.log(sc);
+ 
+//  // appending cities 9(Palo Alto) to the content div, so it will print on the page. 
+//    document.getElementById("content").append(data.cities[9].geoloc.city);
+ 
+//    // Adding the hourly temperature and condition for Palo Alto on Friday to the content div. 
+//    document.getElementById("content").append(cities[9].current[0].temp ,'°');
+//    document.getElementById("content").append(cities[9].current[0].condition);
+     
+    
+  
+//     //  Weekly Forecast
+//       var daily= cities[9].weekly;
+//        console.log(daily);
+       
+//        // Used the forEach method to have each day individually print to the console.
+//        daily.forEach(element => {
+//         console.log(element.day);
+//          if(element => 10){
+//            console.log(element.daycondition );
+//         }
+//         //  Adding days of the week and Weather condition.
+//         document.getElementById("forecast").append(element.day , ':' ); 
+//          document.getElementById("forecast").append(element.daycondition , ' ' , '  ');
+
+        
+//        });
+    
+//     };
+  
+  
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
